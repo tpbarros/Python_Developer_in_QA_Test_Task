b3b0aaf6ddc606a96ab44302a557dd527ef8fb52
tpbarros <50621994+tpbarros@users.noreply.github.com>
2024-02-04 12:28:57 +0000

    add synchTimeLoop function
    create a new function to call the synching fo the two folders every time interval.

--- Test_task_tomas_barros.py
+++ Test_task_tomas_barros.py
@@ -8,10 +8,39 @@ Veeam test task
 """
 
 import sys
+import time
+
+
+def synchTimeLoop(sourceFolderPath, replicaFolderPath, synchInterval,
+                  logFilePath):
+    '''Preforms synchronization every synchInterval seconds
+    
+    Agrs:
+        sourceFolderPath: the path of the source folder
+        replicaFolderPath: the path of the replica folder
+        synchInterval: how much time passes between synchronization
+        sourceFolderPath: the path of the source folder
+    '''
+    # it keeps synchromizing the two folders each synchInterval seconds until
+    # the program is manually stopped
+    while True:
+        try:
+            time.sleep(synchInterval)
+            synchFolders(sourceFolderPath, replicaFolderPath, synchInterval,
+                              logFilePath)
+        except KeyboardInterrupt:
+            print("Synchronization terminated.")
+            raise SystemExit
+
+def synchFolders(sourceFolderPath, replicaFolderPath, synchInterval,
+                logFilePath):
+    pass
 
 # Read folder paths, synchronization interval and log file path from command
 # line arguments, in that order
 sourceFolderPath = sys.argv[1]
 replicaFolderPath = sys.argv[2]
-synchInterval = sys.argv[3]
+synchInterval = int(sys.argv[3])
 logFilePath = sys.argv[4]
+
+synchTimeLoop(synchInterval)
